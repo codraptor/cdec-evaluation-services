ee387e9ee5d375640b06cd781ae71df01bc800cd
rishubh <rishubhngp@gmail.com>
2021-12-19 02:32:35 -0500

    Increase token validity

--- src/main/java/com/cdec/validator/utils/JWTUtil.java
+++ src/main/java/com/cdec/validator/utils/JWTUtil.java
@@ -24,7 +24,7 @@ public class JWTUtil {
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 500))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 6000))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
